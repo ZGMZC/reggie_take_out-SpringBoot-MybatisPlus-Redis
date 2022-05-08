3a286a6b5051d3b5a40f00f2991a30515485ff58
ZGMZC <zhuguangmao1002@foxmail.com>
2022-05-08 20:08:29 +0800

    v1.2 SpringCache

--- src/main/java/com/process2/reggie/common/R.java
+++ src/main/java/com/process2/reggie/common/R.java
@@ -2,11 +2,12 @@ package com.process2.reggie.common;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
-public class R<T> {
+public class R<T> implements Serializable {
     private Integer code; //编码：1成功，0和其它数字为失败
 
     private String msg; //错误信息
--- src/main/java/com/process2/reggie/controller/SetmealController.java
+++ src/main/java/com/process2/reggie/controller/SetmealController.java
@@ -13,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +33,7 @@ public class SetmealController {
     private CategoryService categoryService;
 
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
 
         setmealService.saveWithDish(setmealDto);
@@ -83,6 +86,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
 //        log.info("ids：{}",ids);
         setmealService.removeWithDish(ids);
@@ -94,6 +98,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("list")
+    @Cacheable(value = "setmealCache",key ="#setmeal.categoryId+'_'+#setmeal.status" )
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
